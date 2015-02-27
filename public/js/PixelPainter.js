fe39b532fc44990ace74270c5dfea3dcabce72ed
Jesse Copeland <jesse.copeland@gmail.com>
2015-02-27 13:16:46 -1000

    push before break

--- public/js/PixelPainter.js
+++ public/js/PixelPainter.js
@@ -1,15 +1,15 @@
 // Document is ready
 $(function () {
-  var pixelPainter = new PixelPainter(35,35);
+  var pixelPainter = new PixelPainter(20,20);
   $("#controls").append(pixelPainter.controls());
-  $("#controls").append(pixelPainter.controlBtn());
   $("#artboard").append(pixelPainter.artboard());
 });
 
 function PixelPainter (width, height) {
-  this.width = width;
-  this.height = height;
-  this.currentColor = '#fff';  
+  var self = this;
+  self.width = width;
+  self.height = height;
+  self.currentColor = '#000';
 }
 
 PixelPainter.prototype.buildGrid = function(columns, rows, gridEl, isColorSwatch) {
@@ -30,7 +30,7 @@ PixelPainter.prototype.buildRow = function(columns, rowNumber, isColorSwatch) {
         } else {
           color = this.getColor(rowNumber, 'greys');
         }
-        $cell.css({'background-color': color});
+        $cell.css({'background-color': color}).attr('data-color', color);
       }
       $row.append($cell);
     }
@@ -41,8 +41,15 @@ PixelPainter.prototype.buildRow = function(columns, rowNumber, isColorSwatch) {
 PixelPainter.prototype.controls = function() {
   var $controlsContainer = $('<div>', {'id': 'controls-container'});
   var $colorSwatch = $('<div>', {'id': 'color-swatch'});
+  var $currentColor = $('<div>', {'id': 'current-color', 'data-color': '#000'});
   this.buildGrid(6, 11, $colorSwatch, true);
-  $controlsContainer.append($colorSwatch);
+  $controlsContainer.append($currentColor).append($colorSwatch);
+
+  $colorSwatch.on('click', '.cell', function () {
+    var color = $(this).data('color'); 
+    self.currentColor = color;
+    $('#current-color').data('color', self.currentColor).css({'background-color': self.currentColor});
+  });
   
   return $controlsContainer;
 };
@@ -61,11 +68,11 @@ PixelPainter.prototype.artboard = function() {
 
   //applies color to artboard
   $artboardGrid.on('click', '.cell', function () {
-    $(this).css({'background-color': '#000'});
+    $(this).css({'background-color': self.currentColor});
   });
   $artboardGrid.on('mouseover', '.cell', function () {
     if (isMouseDown) {
-      $(this).css({'background-color': 'rgba(0,0,0,1)'});
+      $(this).css({'background-color': self.currentColor});
     }
   });
   $artboardContainer.append($artboardGrid);
@@ -97,8 +104,6 @@ PixelPainter.prototype.controlBtn = function(){
   
 };
 
-
-
 PixelPainter.prototype.getColor = function(indx, pallet) {
   var colorSets = {
     greys: [
@@ -115,61 +120,17 @@ PixelPainter.prototype.getColor = function(indx, pallet) {
       '#ffffff'
     ],
     colors: [
-      '#cee4f6',
-      '#93c8f6',
-      '#58adf6',
-      '#1d91f6',
-      '#0084f6',
-      '#d1cef6',
-      '#9a93f6',
-      '#6458f6',
-      '#2d1df6',
-      '#1300f6',
-      '#eacef6',
-      '#d893f6',
-      '#c758f6',
-      '#b51df6',
-      '#ac00f6',
-      '#f6cedd',
-      '#f693b9',
-      '#f65895',
-      '#f61d70',
-      '#f6005f',
-      '#f6d5ce',
-      '#f6a493',
-      '#f67458',
-      '#f6431d',
-      '#f62b00',
-      '#f5d1b7',
-      '#f5af7c',
-      '#f58d41',
-      '#f56c07',
-      '#f56800',
-      '#f5e0b7',
-      '#f5cc7c',
-      '#f5b941',
-      '#f5a507',
-      '#f5a200',
-      '#f5efb7',
-      '#f5e97c',
-      '#f5e441',
-      '#f5de07',
-      '#f5dd00',
-      '#ebf5b7',
-      '#e1f57c',
-      '#d7f541',
-      '#cef507',
-      '#cdf500',
-      '#adf4a2',
-      '#94f485',
-      '#7af468',
-      '#47f42d',
-      '#20f400',
-      '#d6f1f7',
-      '#b9ecf7',
-      '#7de1f7',
-      '#42d7f7',
-      '#00cbf7'
+      '#cee4f6','#93c8f6','#58adf6','#1d91f6','#0084f6',
+      '#d1cef6','#9a93f6','#6458f6','#2d1df6','#1300f6',
+      '#eacef6','#d893f6','#c758f6','#b51df6','#ac00f6',
+      '#f6cedd','#f693b9','#f65895','#f61d70','#f6005f',
+      '#f6d5ce','#f6a493','#f67458','#f6431d','#f62b00',
+      '#f5d1b7','#f5af7c','#f58d41','#f56c07','#f56800',
+      '#f5e0b7','#f5cc7c','#f5b941','#f5a507','#f5a200',
+      '#f5efb7','#f5e97c','#f5e441','#f5de07','#f5dd00',
+      '#ebf5b7','#e1f57c','#d7f541','#cef507','#cdf500',
+      '#adf4a2','#94f485','#7af468','#47f42d','#20f400',
+      '#d6f1f7','#b9ecf7','#7de1f7','#42d7f7','#00cbf7'
     ]
   };
   return colorSets[pallet][indx];
