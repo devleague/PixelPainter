8bf5f26b3181f6c63a5f6718a0ea79502566fc64
jmt75200 <joelle@pcgamerzhawaii.com>
2015-02-26 19:59:22 -1000

    adding sass

---
+++ public/js/PixelPainter.js
@@ -0,0 +1,19 @@
+//jQuery to print to dom
+//action for selection on load
+
+$(function (){
+
+
+});
+
+//create class Pixelpainter
+//accepts two parameters: width, height
+function PixelPainter (width, height){
+  this.width = width;
+  this.height = height;
+}
+
+PixelPainter.prototype.controls = function() {
+  // body...
+};
+
