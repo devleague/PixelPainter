6f095200608a331749066508c93334154f2920e1
Christie R <creindle@hawaii.edu>
2016-06-02 20:12:31 -1000

    I'm dyingD:

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,3 +1,39 @@
-function createGrid() {
+document.body.onload = createGrid;
 
+function createGrid(rows, columns, attributes) {
+  if (rows < 0 || typeof rows !== 'number'){
+    throw new Error();
+  }
+
+  var gridElement = document.createElement("div");
+
+  //create the row
+  for (var i = 0; i <= rows; i++){
+    var row = document.createElement("div");
+    row.className = "row";
+    gridElement.appendChild(row);
+
+    for (var j = 0; j <= columns; j++){
+    var column = document.createElement("div");
+    column.className = "column";
+    row.appendChild(column);
+    }
+  }
+
+  //create the elements in the row
+
+  //column = row;
+
+  //Should return the same number of rows as provided
+
+  //document.getElementByID("pixelPainter").appendChild(column);
+
+  //console.log("Columns" + column);
+  //console.log("Rows" + row);
+
+  console.log(HTMLElement);
+
+  return gridElement;
 }
+
+
--- js/PixelPainter.spec.js
+++ js/PixelPainter.spec.js
@@ -1,4 +1,6 @@
+var assert = chai.assert;
 var expect = chai.expect;
+var should = chai.should;
 /**
  * Creates a grid of elements.
  * @param {number} rows – The number of rows in the grid.
@@ -11,68 +13,23 @@ var expect = chai.expect;
 - There should be a function called `createGrid`.
 */
 describe('createGrid', function(){
-
+  it('should be a function', function(){
+    assert.isFunction(createGrid, "hahahahahahah");
+    assert.isNotObject(createGrid, 'This is wrongggg:D');
+  });
+  it('should return an "HTMLElement"', function() {
+    expect(createGrid(1)).to.be.an.instanceOf(HTMLElement);
+  });
   describe('rows', function() {
     it('should only accept a non-negative number', function() {
-      expect(createGrid).to.be.a.function;
-      expect(createGrid({}).bind.to.throw(Error));
-      //expect(createGrid(-1).should.not.throw(Error));
-      //expect(createGrid(1).should.not.throw(Error));
+      expect(createGrid.bind(null)).to.throw();
+      expect(createGrid.bind(null, {})).to.throw();
+      expect(createGrid.bind(null, -1)).to.throw();
+      expect(createGrid.bind(null, 1)).to.not.throw();
     });
-    it.skip('should return an "HTMLElement"', function() {
-      expect(createGrid(1).to.equal("HTMLElement"));
+    it('should return the same number of rows as provided', function() {
+      //createGrid(1) Should return an HTMLElement with 1 child.
+      expect(createGrid(1)).to.satisfy(function(){return HTMLElement;});
     });
   });
 });
-//     it('should return the same number of rows as provided', function() {
-//       expect(createGrid(1).to.equal("HTMLElement") with 1 child
-//       `createGrid(10)` Should return an html element with 10 children
-//     });
-
-//   describe.skip('columns', function() {
-//     it('If no column value is provided, columns should equal the number of rows', function() {
-//       expect(createGrid().bind(null, "1", "1")).to.throw(Error);
-//     });
-//     it('If a column value is provided, that should be the number of columns returned', function() {
-//       expect(createGrid().bind(null, "1", "1")).to.throw(Error);
-//     });
-//   });
-
-//   describe.skip('attributes', function() {
-//   it('If no column value is provided, all attributes should get applied to each grid element', function() {
-//     expect(createGrid().bind(null, "1", "1")).to.throw(Error);
-//   });
-//   it('If a column value is provided, that should be the number of columns returned, along with the attributes applied.', function() {
-//     expect(createGrid().bind(null, "1", "1")).to.throw(Error);
-//   });
-// });
-
-// - `createGrid` should return an `HTMLElement`.
-
-// - `rows`
-//   - Should only accept be a non-negative number
-//     - `createGrid()` should throw a `Error`.
-//     - `createGrid({})` should throw a `Error`.
-//     - `createGrid(-1)` should *not* throw an `Error`.
-//     - `createGrid(1)` should *not* throw an `Error`.
-//   - Should return an `HTMLElement`
-//     - `createGrid(1)` should return an `HTMLElement`.
-//   - Should return the same number of rows as provided.
-//     - `createGrid(1)` Should return an `HTMLElement` with 1 child.
-//     - `createGrid(10)` Should return an html element with 10 children.
-
-// - `columns`
-//   - If no column value is provided, columns should equal the number of rows.
-//     - `createGrid(1)` Should return an html element with 1 row, each with 1 column.
-//     - `createGrid(10)` Should return an html element with 10 rows, each with 10 columns.
-//   - If a column value is provided, that should be the number of columns returned.
-//     - `createGrid(1, 10)` Should return an html element with 1 row, each with 10 columns.
-//     - `createGrid(10, 5)` Should return an html element with 10 rows, each with 5 columns.
-
-// - `attributes`
-//   - If no column value is provided, all attributes should get applied to each grid element
-//     - `createGrid(1, {})` Should return an html element with 1 row, each with 1 column, each with no attributes.
-//     - `createGrid(10, {class: 'grid'})` Should return an html element with 10 rows, each with 10 columns, each with the `class` of `grid`.
-//     - `createGrid(10, {class: 'grid', style: 'background-color: black'})` Should return an html element with 10 rows, each with 10 columns, each with the `class` of `grid` and `style` of 'background-color: black'.
-//   - If a column value is provided, that should be the number of columns returned, along with the attributes applied.
-//     - `createGrid(10, 5, {class: 'grid'})` Should return an html element with 10 rows, each with 5 columns, each with the `class` of `grid`.
