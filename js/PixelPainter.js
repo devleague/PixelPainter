6b88e100748fba072481c5f24f1b88957d15df2e
sgalutira <sheena.galutira@gmail.com>
2016-06-04 13:44:52 -1000

    columns parameter

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,14 +1,21 @@
 function createGrid(rows, columns, attributes) {
   var gridElement = document.createElement('div');
   gridElement.className = "grid";
-
   if (typeof rows !== 'number' || rows < 0){
     throw new Error();
   }
   for (var i = 0; i < rows; i++){
     var rowElement = document.createElement('div');
-    rows.ClassName = "row";
+    rowElement.className = "row";
     gridElement.appendChild(rowElement);
-  }
+    if (columns === undefined){
+      columns = rows;
+    }
+      for (var j = 0; j < columns; j++){
+        var columnElement = document.createElement('div');
+        columnElement.className = "column";
+        rowElement.appendChild(columnElement);
+      }
+   }
   return gridElement;
 }
