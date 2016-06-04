1cdce0dcd491f89e1187a639f02c37641a4daf36
sgalutira <sheena.galutira@gmail.com>
2016-06-03 17:52:58 -1000

    rows parameter: throw error and create row

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,6 +1,14 @@
-function createGrid(rows, columns, attributes){
+function createGrid(rows, columns, attributes) {
   var gridElement = document.createElement('div');
   gridElement.className = "grid";
+
+  if (typeof rows !== 'number' || rows < 0){
+    throw new Error();
+  }
+  for (var i = 0; i < rows; i++){
+    var rowElement = document.createElement('div');
+    rows.ClassName = "row";
+    gridElement.appendChild(rowElement);
+  }
   return gridElement;
 }
-createGrid();
