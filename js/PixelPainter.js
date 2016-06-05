8b8c5641b2640a5407265216dca6c845a8b141dc
sgalutira <sheena.galutira@gmail.com>
2016-06-04 19:25:37 -1000

    attributes test passed

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,21 +1,45 @@
 function createGrid(rows, columns, attributes) {
   var gridElement = document.createElement('div');
-  gridElement.className = "grid";
   if (typeof rows !== 'number' || rows < 0){
     throw new Error();
   }
+  // if (typeof columns === 'object'){
+  //   attributes = columns;
+  // }
+  if (typeof columns !== 'number' || columns < 0){
+    if (typeof columns === 'object'){
+      attributes = columns;
+    }
+    columns = rows;
+  }
+
+  console.log(rows, columns, attributes);
+
   for (var i = 0; i < rows; i++){
     var rowElement = document.createElement('div');
-    rowElement.className = "row";
     gridElement.appendChild(rowElement);
-    if (columns === undefined){
-      columns = rows;
+
+    for (var j = 0; j < columns; j++){
+      var columnElement = document.createElement('div');
+      setAttr(columnElement, attributes);
+      rowElement.appendChild(columnElement);
     }
-      for (var j = 0; j < columns; j++){
-        var columnElement = document.createElement('div');
-        columnElement.className = "column";
-        rowElement.appendChild(columnElement);
-      }
+    // if (typeof attributes === 'object'){
+    //   var keys = Object.keys(attributes);
+    //   for (var k = 0; k < keys.length; k++){
+    //   }
+    // }
    }
   return gridElement;
 }
+
+
+function setAttr(element, attrObj){
+  if (typeof attrObj === 'object'){
+  keys = Object.keys(attrObj);
+  for (var i = 0; i < keys.length; i++){
+    element.setAttribute(keys[i], attrObj[keys[i]]);
+  }
+  }
+}
+createGrid(10, {class: 'grid', style: 'background-color: black'});
