2c95b9096c5de2c8bd4fa6ac5c9b12b55ddf0861
sgalutira <sheena.galutira@gmail.com>
2016-06-07 22:41:01 -1000

    pp-canvas, need to work on color palette and events

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,39 +1,36 @@
 function createGrid(rows, columns, attributes) {
   var gridElement = document.createElement('div');
+  gridElement.className = "grid";
+  // validations
   if (typeof rows !== 'number' || rows < 0){
-    throw new Error();
+    throw new Error("Non-negative values only.");
   }
-  // if (typeof columns === 'object'){
-  //   attributes = columns;
-  // }
   if (typeof columns !== 'number' || columns < 0){
     if (typeof columns === 'object'){
       attributes = columns;
     }
     columns = rows;
   }
-
-  console.log(rows, columns, attributes);
-
+  // loops to create rows and columns
   for (var i = 0; i < rows; i++){
     var rowElement = document.createElement('div');
+    rowElement.className = "rows";
+    setAttr(rowElement, attributes);
     gridElement.appendChild(rowElement);
 
     for (var j = 0; j < columns; j++){
       var columnElement = document.createElement('div');
       setAttr(columnElement, attributes);
+      if (attributes === undefined){
+        columnElement.className = "columns";
+      }
       rowElement.appendChild(columnElement);
     }
-    // if (typeof attributes === 'object'){
-    //   var keys = Object.keys(attributes);
-    //   for (var k = 0; k < keys.length; k++){
-    //   }
-    // }
    }
+
   return gridElement;
 }
-
-
+// function to add attributes to the grid
 function setAttr(element, attrObj){
   if (typeof attrObj === 'object'){
   keys = Object.keys(attrObj);
@@ -42,4 +39,17 @@ function setAttr(element, attrObj){
   }
   }
 }
-createGrid(10, {class: 'grid', style: 'background-color: black'});
+// creating a pixel painter with color palette and grid using createGrid function
+function pixelPainter(width, height, attributes){
+  var paintContainer = document.getElementById('pixelPainter');
+  var colorsGrid = createGrid(6, 11, {class: 'colors'});
+  colorsGrid.id = "pp-colors";
+  paintContainer.appendChild(colorsGrid);
+  var canvasGrid = createGrid(width, height);
+  canvasGrid.id = "pp-canvas";
+  paintContainer.appendChild(canvasGrid);
+  document.innerHTML = paintContainer;
+}
+
+
+
