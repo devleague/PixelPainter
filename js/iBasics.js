b41cc1014fb00f1a8f07bb342ceeb99eb1719896
Edward Gao <egao@sent.com>
2016-11-20 00:32:55 -1000

    manage iBasics in separate .js file

---
+++ js/iBasics.js
@@ -0,0 +1,8 @@
+var iBasics = (function(){
+  function _map(item) {
+
+  }
+  return{
+    map: _map
+  };
+})();
--- js/iQuery.js
+++ js/iQuery.js
@@ -5,11 +5,21 @@ var FIRST_ITEM = 0;
 var ID_SELECTOR = '#';
 var CLASS_SELECTOR = '.';
 
+// variables
+
+
 // selector (simulating jQuery)
-function $(elementName, index) {
+function $(elementName) {
   elementName = elementName.trim();
   var firstChar = elementName.charAt(FIRST_CHAR);
   var selection;
+
+  // when no index is passed in
+  if(index === undefined) {
+    index = 1;
+  }
+
+  // select class or id
   if(firstChar === ID_SELECTOR) {
     elementName = elementName.substr(FROM_SECOND_CHAR);
     selection = document.getElementById(elementName);
@@ -18,8 +28,22 @@ function $(elementName, index) {
     selection = document.getElementsByClassName(elementName);
   }
 
+  // event handler
   function onEvent(device, action, myFunction) {
     if(firstChar === ID_SELECTOR) {
+
+    }else if(firstChar === CLASS_SELECTOR){
+      if(device === 'mouse') {
+        mouseEventsForClass(action, myFunction);
+      }
+    }
+  }
+
+  // event handler -> class -> mouse
+  function mouseEventsForClass(action, myFunction) {
+
+    iBasics.map(action).from(actionList).to(mouseActions);
+    for(var i = 0; i < selection.length; i++) {
 
     }
   }
