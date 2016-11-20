9e46e2bf6eff8a2f79f3fe835d4a07cebbabde0a
Edward Gao <egao@sent.com>
2016-11-19 21:24:12 -1000

    modularize div frames generator

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,26 +1,34 @@
-console.log("creating outer divs works");
+// iife that creates basic framing
+var mainContainer = (function() {
+  var outerFrame = document.createElement('div');
+  outerFrame.className = 'outer-frame';
 
-function createDivFrames(){
-  var outerFrame = document.createElement("div");
-  outerFrame.className = "outer-frame";
-
-  var controlPanel = document.createElement("control");
-  controlPanel.className = "control";
+  var controlPanel = document.createElement('div');
+  controlPanel.className = 'control';
   outerFrame.appendChild(controlPanel);
 
-  var palette = document.createElement("palette");
-  palette.className = "palette";
+  var palette = document.createElement('div');
+  palette.className = 'palette';
   controlPanel.appendChild(palette);
 
-  var buttons = document.createElement("buttons");
-  buttons.className = "buttons";
+  var buttons = document.createElement('div');
+  buttons.className = 'buttons';
   controlPanel.appendChild(buttons);
 
-  var grid = document.createElement("grid");
-  grid.className = "grid";
+  var grid = document.createElement('div');
+  grid.className = 'grid';
   outerFrame.appendChild(grid);
 
   document.body.appendChild(outerFrame);
-}
 
-createDivFrames();
+  return {
+    outerFrame: outerFrame,
+    controlPanel: controlPanel,
+    palette: palette,
+    buttons: buttons,
+    grid: grid
+  };
+});
+
+// run and assign to variable named 'page'
+var page = mainContainer();
