2ac8e783fad819525eefe36a26f9810b8e5de56c
Naoto Yamaguchi <naotoy@hawaii.edu>
2016-11-19 20:04:17 -1000

    all the work we did together in class

---
+++ js/PixelPainter.js
@@ -0,0 +1,66 @@
+var mainBody = document.getElementById('pixelPainter');
+var colorArray = ['red', 'blue', 'green', 'purple', 'yellow', 'pink'];
+var colorChoice = 'black';
+var colorButtons = [];
+var currentColorOfLoop;
+
+function createColorGrid(){
+
+
+  function miscButtons(){
+    var miscButtons = document.createElement('div');
+    miscButtons.className = "miscButtons";
+    mainBody.appendChild(miscButtons);
+    var clearButton = document.createElement('button');
+    miscButtons.appendChild(clearButton);
+    clearButton.addEventListener('click', function(){
+      cell.style.backgroundColor = 'white';
+    });
+  }
+
+  function makePallete(){
+    var pallete = document.createElement('div');
+    pallete.className = "pallete";
+    mainBody.appendChild(pallete);
+    for(var j = 0; j < colorArray.length; j++){
+    currentColorOfLoop = colorArray[j];
+    colorButtons[j] = document.createElement('div');
+    colorButtons[j].className = "palleteButton";
+    pallete.appendChild(colorButtons[j]);
+    colorButtons[j].style.width = 20;
+    colorButtons[j].style.height = 20;
+    colorButtons[j].style.backgroundColor = currentColorOfLoop;
+    colorButtons[j].addEventListener('click', function(n){
+      colorChoice = this.style.backgroundColor;
+    });
+  }
+  }
+
+  function makeGrid(rows, cols){
+    var grid = document.createElement('table');
+    grid.className = "grid";
+    mainBody.appendChild(grid);
+
+    for(var i = 0; i<rows; i++){
+      var tr = grid.appendChild(document.createElement('tr'));
+      for(var j = 0; j<cols; j++){
+        var cell = tr.appendChild(document.createElement('td'));
+        cell.className = "pixels";
+        cell.style.backgroundColor = 'gray';
+        cell.style.width = 20;
+        cell.style.height = 20;
+        cell.addEventListener('click', function(){
+          this.style.backgroundColor = colorChoice;
+        });
+      }
+    }
+  }
+  // return makeGrid(40,40);
+  return {
+    makeGrid: makeGrid(10,10),
+    makePallete: makePallete(),
+    miscButtons: miscButtons()
+  };
+}
+
+var ll = createColorGrid();
