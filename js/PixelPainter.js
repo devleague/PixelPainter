349f7ed87ae5e32767a465421f81190433c6fea5
nms377 <nms377@nyu.edu>
2016-11-22 21:57:27 -1000

    Added a canvas to paint on.

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,4 +1,14 @@
-( function grid() {
+function grid() {
   //some line of code
-})();
+
+//Creating the Canvas to Paint On
+var canvas = document.createElement('canvas');
+canvas.id = 'grid';
+canvas.height = 600;
+canvas.width = 600;
+canvas.style.border = '1px solid';
+pixelPainter.appendChild(canvas);
+}
+
+grid();
 
