9d46f36310a4e2f47e9bf059236741f4a2ec2028
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-11-23 21:11:37 -1000

    working on css

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,116 +1,105 @@
-//function pixelPainter (width, height) {
-  var pixelPainter = document.createElement('div');
-  pixelPainter.id = 'palettte';
-  document.body.appendChild(pixelPainter);
+var pixelPainter = document.createElement('div');
+pixelPainter.id = 'palette';
+document.body.appendChild(pixelPainter);
 
-  var pixelPainterCanvas = document.createElement('div');
-  var colorSwatchesCanvas = document.createElement('div');
+var pixelPainterCanvas = document.createElement('div');
+var colorSwatchesCanvas = document.createElement('div');
 
-  var whenClicked = null;
+var whenClicked = null;
 
-  var color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'brown', 'gray'];
+var color = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'black', 'brown', 'gray'];
 
-  pixelPainterCanvas.className = 'ppCanvas';
-  pixelPainter.appendChild(pixelPainterCanvas);
+pixelPainterCanvas.className = 'ppCanvas';
+pixelPainter.appendChild(pixelPainterCanvas);
 
-  colorSwatchesCanvas.className = 'colorCanvas';
-  pixelPainter.appendChild(colorSwatchesCanvas);
+colorSwatchesCanvas.className = 'colorCanvas';
+pixelPainter.appendChild(colorSwatchesCanvas);
 
-  //Swatch Canvas
-  function swCanvas(pixelSize) {
-    for (var x = 0; x < pixelSize; x++) {
-      var swatchCanvasPixel = document.createElement('div');
-      swatchCanvasPixel.className = 'swatchCanvasPixel';
-      colorSwatchesCanvas.appendChild(swatchCanvasPixel);
-      swatchCanvasPixel.style.backgroundColor = color[x];
-      swatchCanvasPixel.addEventListener('click', storeColorPicker);
-    }
-     // pixelPainter.appendChild(swatchCanvasPixel);
+function swCanvas(pixelSize) {
+  for (var x = 0; x < pixelSize; x++) {
+    var swatchCanvasPixel = document.createElement('div');
+    swatchCanvasPixel.className = 'swatchCanvasPixel';
+    colorSwatchesCanvas.appendChild(swatchCanvasPixel);
+    swatchCanvasPixel.style.backgroundColor = color[x];
+    swatchCanvasPixel.addEventListener('click', storeColorPicker);
   }
+}
 
-  //Pixel Painter Canvas
-  function ppCanvas(pixelSize) {
-    for (var x = 0; x < pixelSize; x++) {
-      var canvasPixel = document.createElement('div');
-      canvasPixel.className = 'canvasPixelCell';
-      pixelPainterCanvas.appendChild(canvasPixel);
-      // --------- EVENTLISTENERS -----------
-      canvasPixel.addEventListener('click', insertColorPicker);
-      canvasPixel.addEventListener('mousedown', function(event) {mouseDown(event);
-      });
-      canvasPixel.addEventListener('mouseover', function(event) {mouseOver(event);
-      });
-      canvasPixel.addEventListener('mouseup', function(event) {mouseUp(event);
-      });
-
-    }
+function ppCanvas(pixelSize) {
+  for (var x = 0; x < pixelSize; x++) {
+    var canvasPixel = document.createElement('div');
+    canvasPixel.className = 'canvasPixelCell';
+    pixelPainterCanvas.appendChild(canvasPixel);
+    canvasPixel.addEventListener('click', insertColorPicker);
+    canvasPixel.addEventListener('mousedown', function(event) {mouseDown(event);
+    });
+    canvasPixel.addEventListener('mouseover', function(event) {mouseOver(event);
+    });
+    canvasPixel.addEventListener('mouseup', function(event) {mouseUp(event);
+    });
   }
+}
 
-  ppCanvas(2500);
-  swCanvas(10);
+ppCanvas(2500);
+swCanvas(10);
 
-  // // colorPick is supposed to click the color from the swatches and store that vaule
-  function storeColorPicker(event) {
-    currentColor = event.target.style.backgroundColor;
-    console.log(event);
+function storeColorPicker(event) {
+  currentColor = event.target.style.backgroundColor;
+  console.log(event);
+}
+
+function insertColorPicker(event) {
+  if (event.target.style.backgroundColor === 'white') {
+    event.target.style.backgroundColor = currentColor;
+  } else if (event.target.style.backgroundColor !== 'white') {
+    event.target.style.backgroundColor = currentColor;
   }
+}
 
-  function insertColorPicker(event) {
-    if (event.target.style.backgroundColor === 'white') {
-      event.target.style.backgroundColor = currentColor;
-    } else if (event.target.style.backgroundColor !== 'white') {
-      event.target.style.backgroundColor = currentColor;
-    }
+function mouseDown(event) {
+  whenClicked = true;
+  if (whenClicked === true) {
+    event.target.style.backgroundColor = currentColor;
   }
+}
 
-  function mouseDown(event) {
-    whenClicked = true;
-    if (whenClicked === true) {
-      event.target.style.backgroundColor = currentColor;
-    }
+function mouseOver(event) {
+  if (whenClicked === true) {
+    event.target.style.backgroundColor = currentColor;
   }
+}
 
-  function mouseOver(event) {
-    if (whenClicked === true) {
-      event.target.style.backgroundColor = currentColor;
-    }
-    console.log(evt);
+function mouseUp(event) {
+  whenClicked = false;
+  if (whenClicked === false) {
+    event.target.style.backgroundColor = currentColor;
   }
+}
 
-
-  function mouseUp(event) {
-    whenClicked = false;
-    if (whenClicked === false) {
-      event.target.style.backgroundColor = currentColor;
-    }
+function colorContinous(){
+  if('mouseDown'){
+    event.target.style.backgroundColor = currentColor;
   }
+}
 
-  function colorContinous(){
-    if('mouseDown'){
-      event.target.style.backgroundColor = currentColor;
-    }
+var eraseButton = document.createElement('button');
+eraseButton.className = 'buttons';
+eraseButton.innerHTML = "Erase";
+pixelPainter.appendChild(eraseButton);
+eraseButton.addEventListener('click', erase);
+
+var clearButton = document.createElement('button');
+clearButton.className = 'buttons';
+clearButton.innerHTML = "Clear";
+pixelPainter.appendChild(clearButton);
+clearButton.addEventListener('click', clear);
+
+function erase(event) {
+  currentColor = 'white';
+}
+
+function clear(event) {
+  for(var i = 0; i < document.querySelectorAll('.canvasPixelCell').length; i++) {
+    document.querySelectorAll('.canvasPixelCell')[i].style.backgroundColor = 'white';
   }
-
-  var eraseButton = document.createElement('button');
-  eraseButton.innerHTML = "Erase";
-  pixelPainter.appendChild(eraseButton);
-  eraseButton.addEventListener('click', erase);
-
-  var clearButton = document.createElement('button');
-  clearButton.innerHTML = "Clear";
-  pixelPainter.appendChild(clearButton);
-  clearButton.addEventListener('click', clear);
-
-  function erase(event) {
-    currentColor = 'white';
-  }
-
-  function clear(event) {
-    for(var i = 0; i < document.querySelectorAll('.canvasPixelCell').length; i++) {
-        document.querySelectorAll('.canvasPixelCell')[i].style.backgroundColor = 'white';
-    }
-  }
-
-
-
-//}
+}
