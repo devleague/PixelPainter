ecebf8f15ad07b07319a19eb12586ad7e0128d2b
John Atkins <johnatkins1999@yahoo.com>
2017-06-20 11:33:55 -1000

    latest

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -7,6 +7,8 @@ window.PixelPainter = (function(){
   let currColor = "color";
   let e = event;
 
+
+
   pixelPainter.setColor = function(color){
     currColor = color;
   }
@@ -15,7 +17,11 @@ window.PixelPainter = (function(){
     return currColor;
   }
 
-  pixelPainter.createGrid  = function(width, height, className, fill){
+  pixelPainter.createGrid = function(width, height, className, func, fill) {
+    var f;
+    if (func === 1) { f = PixelPainter.assignColor; };
+    if (func === 2) { f = PixelPainter.paint; }
+
     doc.documentElement.style.setProperty("--rowNum", width);
     doc.documentElement.style.setProperty("--colNum", height);
     let mainDiv = doc.createElement("div");
@@ -28,15 +34,20 @@ window.PixelPainter = (function(){
       subDiv.setAttribute("class", classVar);
       subDiv.setAttribute("id", idVar);
       subDiv.setAttribute("style", "background-color:" + fill[i]);
-      subDiv.addEventListener("click", PixelPainter.assignColor);
+      subDiv.addEventListener("click", f);
 
       mainDiv.append(subDiv);
     }
     doc.body.appendChild(mainDiv);
   }
 
+  pixelPainter.paint = function(e){
+      e.currentTarget.style.setPropertyValue("background-color") = pixelPainter.getColor();
+      alert(e.currentTarget.style.setPropertyValue("background-color"));
+  }
+
   pixelPainter.assignColor = function(e){
-        currentColor = e.currentTarget.style.getPropertyValue("background-color");
+        currentColor = e.currentTarget.style.setPropertyValue("background-color");
         currColor = currentColor;
         PixelPainter.setColor(currentColor);
   }
@@ -45,7 +56,11 @@ window.PixelPainter = (function(){
 
 })();
 
-var fill = ["orange", "red", "yellow", "rgb(251, 183, 255)"];
 
-PixelPainter.createGrid(2, 2, "box ", fill);
+  var fill = ["orange", "red", "yellow", "rgb(251, 183, 255)"];
 
+  PixelPainter.createGrid(2, 30, "box ", 1, fill);
+
+  var fill2 = ["white", "white"];
+
+  PixelPainter.createGrid(3, 30, "box ", 2, fill2);
