99ab14d03802d59998141ab1b2486b5c58bcb6cd
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-01-09 23:29:52 -1000

    merge

--- js/canvas.js
+++ js/canvas.js
@@ -1,7 +1,24 @@
 console.log("canvas");
 function canvasModule(){
-   
-    return {
+
+    let ppId = document.getElementById("pixelPainter");
+    let canvId = document.createElement("div");
+    canvId.id = "canvas";
+    ppId.appendChild(canvId);
+    
+    let height = 0;
+    let width = 0;
+
+    let createSpace = function(){
 
     }
+    let getDimensions = function(){
+
+    }
+    return {
+        createSpace: createSpace,
+        getDimensions: getDimensions
+    }
 }
+
+canvasModule();
