098d9e82af7ce5149ae0b9323b19146cfef08871
quinlayen <quinlayen@gmail.com>
2018-01-11 19:29:20 -1000

    fixing merge conflicts

--- js/canvas.js
+++ js/canvas.js
@@ -71,7 +71,6 @@ function canvasModule(){
                 squareEl.style.width = _cellSize;
                 squareEl.style.paddingBottom = _cellSize;
                 canRow.appendChild(squareEl);
-                console.log('here I am')
             }
         }
         let cRow = document.getElementsByClassName("canvasRow");
@@ -180,4 +179,3 @@ function canvasModule(){
 }
 
 canvasModule();
-console.log('now');
