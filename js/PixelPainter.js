6318d77b8643642945956b2558396ad8e79fa701
JEFrady <jamie.e.halliday@gmail.com>
2018-07-09 21:43:18 -1000

    Color select, make grid, and paint functionalities working

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -21,29 +21,30 @@ pixelPainter.appendChild(toolBox);
     inputColor.textContent = 'Color: ';
     toolBox.appendChild(inputColor);
 
-    var colorSelector = document.createElement('input');
-    colorSelector.setAttribute('id', 'bttnColor');
-    colorSelector.setAttribute('type', 'color');
-    inputColor.appendChild(colorSelector);
+        /// add color selector input to div
+        var colorSelector = document.createElement('input');
+        colorSelector.setAttribute('id', 'bttnColor');
+        colorSelector.setAttribute('type', 'color');
+        inputColor.appendChild(colorSelector);
 
     /// add div container for etchasketch buttons to toolbox
     var etchaSketch = document.createElement('div');
     etchaSketch.setAttribute('id', 'etchaSketch');
     toolBox.appendChild(etchaSketch);
 
-    /// add clear button to etchasketch
-    var clearButton = document.createElement('button');
-    clearButton.setAttribute('id', 'bttnClear');
-    clearButton.setAttribute('type', 'submit');
-    clearButton.textContent = 'Clear';
-    etchaSketch.appendChild(clearButton);
+        /// add clear button to etchasketch
+        var clearButton = document.createElement('button');
+        clearButton.setAttribute('id', 'bttnClear');
+        clearButton.setAttribute('type', 'button');
+        clearButton.textContent = 'Clear';
+        etchaSketch.appendChild(clearButton);
 
-    /// add reset button to etchasketch
-    var resetButton = document.createElement('button');
-    resetButton.setAttribute('id', 'bttnReset');
-    resetButton.setAttribute('type', 'submit');
-    resetButton.textContent = 'Reset';
-    etchaSketch.appendChild(resetButton);
+        /// add reset button to etchasketch
+        var resetButton = document.createElement('button');
+        resetButton.setAttribute('id', 'bttnReset');
+        resetButton.setAttribute('type', 'button');
+        resetButton.textContent = 'Reset';
+        etchaSketch.appendChild(resetButton);
 
 /// create canvas div
 var canvasDiv = document.createElement('div');
@@ -56,20 +57,69 @@ pixelPainter.appendChild(canvasDiv);
     gridForm.textContent = 'Set canvas size';
     canvasDiv.appendChild(gridForm);
 
-    /// add height input to form
-    var gridHeight = document.createElement('input');
-    gridHeight.setAttribute('id', 'gridHeight');
-    gridHeight.setAttribute('type', 'number');
-    gridHeight.setAttribute('min', '1');
-    gridHeight.setAttribute('placeholder', 'height');
-    gridForm.appendChild(gridHeight);
+        /// add height input to form
+        var gridHeight = document.createElement('input');
+        gridHeight.setAttribute('id', 'gridHeight');
+        gridHeight.setAttribute('type', 'number');
+        gridHeight.setAttribute('placeholder', 'height');
+        gridForm.appendChild(gridHeight);
 
-    /// add width input to form
-    var gridWidth = document.createElement('input');
-    gridWidth.setAttribute('id', 'gridWidth');
-    gridWidth.setAttribute('type', 'number');
-    gridWidth.setAttribute('min', '1');
-    gridWidth.setAttribute('placeholder', 'width');
-    gridForm.appendChild(gridWidth);
+        /// add width input to form
+        var gridWidth = document.createElement('input');
+        gridWidth.setAttribute('id', 'gridWidth');
+        gridWidth.setAttribute('type', 'number');
+        gridWidth.setAttribute('placeholder', 'width');
+        gridForm.appendChild(gridWidth);
 
+        /// add submit button to form
+        var submitButton = document.createElement('button');
+        submitButton.setAttribute('id', 'bttnSubmit');
+        submitButton.setAttribute('type', 'submit');
+        submitButton.textContent = 'Submit';
+        gridForm.appendChild(submitButton);
 
+    /// add empty table to canvas div
+    var gridTable = document.createElement('table');
+    gridTable.setAttribute('id', 'gridTable');
+    canvasDiv.appendChild(gridTable);
+
+///make the grid
+function makeGrid () {
+    let rowz = document.getElementById('gridWidth').value;
+    let colz = document.getElementById('gridHeight').value;
+
+    /// clears existing grid elements, prevents "adding to" grid
+    while (gridTable.firstChild) {
+        gridTable.removeChild(gridTable.firstChild);
+    }
+
+    for (var i=1; i<=colz; i++) {
+        /// makes rows
+        let gridRow = document.createElement('tr');
+        gridTable.appendChild(gridRow);
+        for (let j=1; j<=rowz; j++) {
+            /// makes columns
+            let gridCell = document.createElement('td');
+            gridRow.appendChild(gridCell);
+
+            /// add color to cells
+            gridCell.addEventListener('mousedown', function () {
+                const color = document.getElementById('bttnColor').value;
+                this.style.backgroundColor = color;
+            })
+        }
+    }
+}
+
+/// gridForm button submit to call makeGrid function
+gridForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    makeGrid();
+
+    /// hide form after submit
+    if (gridForm.style.display === "none") {
+        gridForm.style.display = "block";
+    } else {
+        gridForm.style.display = "none";
+    }
+})
