443179c41a1667e80d4b62d1b867d7849f67d16e
Arthur <kingart808@gmail.com>
2019-07-10 16:15:57 -1000

    finished clear saved working on undo button

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -20,36 +20,31 @@ clear.innerHTML = "Clear";
 save.innerHTML = "Save";
 load.innerHTML = "Load";
 pixelPainter.appendChild(canvas);
-let height = 10;
-let width = 10;
-let savePic = [];
-const canvasData = [];
+let height = 50;
+let width = 50;
+let saveData = [];
+let history = [];
+const cells = document.getElementsByClassName("cell");
 
 for (let i = 0; i < height; i++) {
   let row = document.createElement("div");
   row.className = "row";
   canvas.appendChild(row);
-  let rowData = [];
-  canvasData.push(rowData);
+
   for (let j = 0; j < width; j++) {
     let cell = document.createElement("div");
-    let cellData = null;
-    rowData.push(cellData);
     cell.className = "cell";
     row.appendChild(cell);
     cell.addEventListener("mousedown", function() {
       mousedown = true;
       this.style.backgroundColor = activeColor;
-      canvasData[i][j] = activeColor;
     });
     cell.addEventListener("mouseover", function() {
       if (mousedown === true) {
         this.style.backgroundColor = activeColor;
-        canvasData[i][j] = activeColor;
       }
     });
     cell.addEventListener("mouseup", stopPaint);
-    // canvas.addEventListener("mouseout", stopPaint);
   }
 }
 const colorPalete = [
@@ -64,41 +59,38 @@ const colorPalete = [
   "blue",
   "blueviolet",
   "violet",
-  "mediumvioletred"
+  "mediumvioletred",
+  "black",
+  "white",
+  "gray"
 ];
 save.addEventListener("click", function() {
-  // debugger;
-  savePic = canvasData.map(function(arr) {
-    return arr.slice();
-  });
-});
-load.addEventListener("click", function() {
-  const canvasEl = document.getElementById("canvas");
-  const rowList = canvasEl.getElementsByClassName("row");
-  for (let i = 0; i < height; i++) {
-    const rowEl = rowList[i];
-    const cellList = rowEl.getElementsByClassName("cell");
-    for (let j = 0; j < width; j++) {
-      cellList[j].style.backgroundColor = savePic[i][j];
-    }
+  for (let i = 0; i < cells.length; i++) {
+    saveData[i] = cells[i].style.backgroundColor;
   }
 });
+clearSave.addEventListener("click", function() {
+  for (let i = 0; i < saveData.length; i++) {
+    saveData[i] = "rgba(255, 255, 255, 0.5)";
+  }
+});
+load.addEventListener("click", function() {
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = saveData[i];
+  }
+});
+undo.addEventListener('click',function () {
+  
+})
 clear.addEventListener("click", function() {
-  const canvasEl = document.getElementById("canvas");
-  const rowList = canvasEl.getElementsByClassName("row");
-  for (let i = 0; i < height; i++) {
-    const rowEl = rowList[i];
-    const cellList = rowEl.getElementsByClassName("cell");
-    for (let j = 0; j < width; j++) {
-      cellList[j].style.backgroundColor = null;
-      canvasData[i][j] = null;
-    }
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "rgba(255, 255, 255, 0.5)";
   }
 });
 const colors = document.createElement("div");
 colors.className = "colors";
 pixelPainter.appendChild(colors);
-const colorHeight = 4;
+const colorHeight = 5;
 const colorwidth = 3;
 for (let x = 0; x < colorHeight; x++) {
   let colorRow = document.createElement("div");
@@ -109,12 +101,12 @@ for (let x = 0; x < colorHeight; x++) {
     colorCols.className = "colorCols";
     colorRow.appendChild(colorCols);
     colorCols.style.backgroundColor = colorPalete.pop();
-    colorCols.addEventListener("click", saveMemory);
+    colorCols.addEventListener("click", makeActiveColor);
   }
 }
 let activeColor;
 let mousedown = false;
-function saveMemory() {
+function makeActiveColor() {
   activeColor = this.style.backgroundColor;
 }
 function stopPaint() {
